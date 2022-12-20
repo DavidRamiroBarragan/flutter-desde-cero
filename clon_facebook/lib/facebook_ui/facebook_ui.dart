4bf35a813cbc1d15d3cbe1c7b6ae625974c53edc
M. David Ramiro <dramirobarragan@gmail.com>
2022-12-20 13:30:59 +0000

    add circle icons and badge

--- clon_facebook/lib/facebook_ui/facebook_ui.dart
+++ clon_facebook/lib/facebook_ui/facebook_ui.dart
@@ -1,10 +1,60 @@
+import 'package:clon_facebook/icons/custom_icons_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'widgets/circle_button.dart';
 
 class FacebookUI extends StatelessWidget {
   const FacebookUI({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: SvgPicture.asset(
+          'assets/logos/facebook.svg',
+          color: Colors.blueAccent,
+          width: 150,
+        ),
+        leadingWidth: 150,
+        systemOverlayStyle: const SystemUiOverlayStyle(
+          statusBarBrightness: Brightness.light,
+        ),
+        actions: const [
+          CircleButton(
+            iconData: CustomIcons.search,
+            color: Color(0xffBFBFBF),
+          ),
+          SizedBox(
+            width: 18,
+          ),
+          CircleButton(
+            iconData: CustomIcons.bell,
+            color: Color(0xffFE7574),
+          ),
+          SizedBox(
+            width: 18,
+          ),
+          CircleButton(
+            iconData: CustomIcons.user_friends,
+            color: Color(0xff7BBAFF),
+            showBadge: true,
+          ),
+          SizedBox(
+            width: 18,
+          ),
+          CircleButton(
+            iconData: CustomIcons.messenger,
+            color: Color(0xff1C86E4),
+          ),
+          SizedBox(
+            width: 18,
+          ),
+        ],
+      ),
+    );
   }
 }
