7c2a827cb9e64629b09110e96bd2babad72c1792
M. David Ramiro <dramirobarragan@gmail.com>
2023-01-18 15:01:26 +0100

    add multi provider widget

--- tv/lib/main.dart
+++ tv/lib/main.dart
@@ -3,10 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 import 'app/data/http/http.dart';
+import 'app/data/repositories_implementation/account_repository_impl.dart';
 import 'app/data/repositories_implementation/authentication_repository_impl.dart';
 import 'app/data/repositories_implementation/connectivity_repository_impl.dart';
 import 'app/data/services/remote/authentication_api.dart';
 import 'app/data/services/remote/internet_checker.dart';
+import 'app/domain/repositories/account_repository.dart';
 import 'app/domain/repositories/authentication_repository.dart';
 import 'app/domain/repositories/connectivity_repository.dart';
 import 'package:provider/provider.dart';
@@ -16,25 +18,28 @@ import 'package:http/http.dart' as http;
 import 'app/my_app.dart';
 
 void main() {
-  runApp(
-    Provider<ConnectivityRepository>(
-      create: (BuildContext context) {
-        return ConnectivityRepositoryImpl(
+  runApp(MultiProvider(
+    providers: [
+      Provider<AccountRepository>(create: (_) => AccountRepositoryImpl()),
+      Provider<ConnectivityRepository>(
+        create: (__) => ConnectivityRepositoryImpl(
           Connectivity(),
           InternetChecker(),
-        );
-      },
-      child: Provider<AuthenticationRepository>(
-        create: (context) => AuthenticationRepositoryImpl(
-          const FlutterSecureStorage(),
-          AuthenticationApi(Http(
-            baseUrl: 'https://api.themoviedb.org/3',
-            apiKey: '515f378a95ecec04875b1d0cefac12df',
-            client: http.Client(),
-          )),
         ),
-        child: const MyApp(),
       ),
-    ),
-  );
+      Provider<AuthenticationRepository>(
+        create: (___) => AuthenticationRepositoryImpl(
+          const FlutterSecureStorage(),
+          AuthenticationApi(
+            Http(
+              baseUrl: 'https://api.themoviedb.org/3',
+              apiKey: '515f378a95ecec04875b1d0cefac12df',
+              client: http.Client(),
+            ),
+          ),
+        ),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
